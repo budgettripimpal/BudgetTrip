02e9548eb48864eb5e7176d067073362b7a12d87
daniriadingoding <daniriadi16@gmail.com>
2025-11-18 21:58:33 +0700

    Implementasi Defensive Programming pada Login dan Register

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,8 +1,12 @@
 @props(['disabled' => false])
 
+@php
+$hasError = $errors->has($attributes->get('name'));
+@endphp
+
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-    'class' => 'border-gray-300 bg-white text-gray-900 
-                focus:border-teal-500 focus:ring-teal-500 
-                rounded-lg shadow-sm w-full'
-    // Kode ini menghapus style dark mode bawaan
+    'class' => 'border-gray-300 bg-white text-gray-900 rounded-lg shadow-sm w-full ' .
+               ($hasError 
+                   ? 'border-red-500 focus:border-red-500 focus:ring-red-500 text-red-900' 
+                   : 'focus:border-teal-500 focus:ring-teal-500')
 ]) !!}>
